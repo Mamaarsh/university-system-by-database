aabdcd85789b8cb5d29e02ae978c548842162b70
mamarsh <aj.silent.84@gmail.com>
2025-03-10 07:17:09 +0330

    Edition-4

--- university_system.py
+++ university_system.py
@@ -17,36 +17,43 @@ class university:
         listlname = [item for item in snamedata]
         self.studentlastname = listlname
         self.studentgrades = []
-        self.studentgpu = 0
+        self.studentgpa = 0
+        self.studentcourses = []
 
 
 
-    def insert(self, studentid, studentname, studentlastname, studentgrades):
+    def insert(self, studentid, studentname, studentlastname, studentgrades, studentcurses):
         self.studentid = studentid
         self.studentname = studentname
         self.studentlastname = studentlastname
         n = len(studentgrades)
         for i in range(n):
             self.studentgrades.append(studentgrades[i])
+            self.studentcourses.append(studentcurses[i])
+
         print("Want add into data base?(y/n)")
         if input().lower() == 'y':
             university.adddatabase(self)
         else:
             print("\nOk")
 
-    def calculategpu(self):
+    def calculategpa(self):
         for i in self.studentgrades:
-            self.studentgpu += int(i)
-        self.studentgpu /= len(self.studentgrades)
-        print("\nGPU is ", self.studentgpu)
+            self.studentgpa += int(i)
+        self.studentgpa /= len(self.studentgrades)
+        print("\nGPU is ", self.studentgpa)
 
     def adddatabase(self):
         print("Are you calculate gpu?(y/n)")
         if input().lower() == 'n':
-            university.calculategpu(self)
-        db = 'insert into students(ids, sfname, slname, gpu) values(%s,%s,%s,%s)'
-        value = (self.studentid, self.studentname, self.studentlastname, self.studentgpu)
+            university.calculategpa(self)
+        db = 'insert into students(ids, sfname, slname, gpa) values(%s,%s,%s,%s)'
+        value = (self.studentid, self.studentname, self.studentlastname, self.studentgpa)
         cur.execute(db, value)
+        for course, grade in zip(self.studentcourses, self.studentgrades):
+            course_query = 'insert into course(ids, coname, score) values(%s,%s,%s)'
+            course_value = (self.studentid, course, grade)
+            cur.execute(course_query, course_value)
         conn.commit()
         print("Added successfully")
 
@@ -59,6 +66,23 @@ class university:
         else:
             print("No data exist!")
 
+    def searchstudents(self):
+        print("Please enter student id: ")
+        inputsearch = input()
+        query = 'select students.ids, students.sfname, students.slname, students.gpa, course.coname, course.score \
+                 from students join course on students.ids = course.id where students.ids'
+        cur.execute(query, (inputsearch,))
+        datas = cur.fetchall()
+        if datas:
+            print(f"\nStudent ID: {datas[0][0]}")
+            print(f"Name: {datas[0][1]} {datas[0][2]}")
+            print(f"GPA: {datas[0][3]}")
+            print("\nCourses:")
+            for row in datas:
+                print(f"- {row[4]} (Score: {row[5]})")
+        else:
+            print("\nNo data exists!")
+
     def deletestudent(self, studentid):
         deleted = False
         orgstid = '('+studentid+',)'
@@ -66,6 +90,7 @@ class university:
             if str(i) == orgstid:
                 cur.execute("delete from students where ids = %s",(studentid))
                 conn.commit()
+                self.studentid.remove(studentid)
                 print("Deleted successfully")
                 deleted = True
                 break
@@ -79,18 +104,18 @@ class university:
 
     def averageofuniversity(self, choose):
         if choose == 1:
-            cur.execute("select avg(gpu) from students")
+            cur.execute("select avg(gpa) from students")
             data = cur.fetchall()
             if data[0][0] is not None:
                 print("Academic average of the university is: ", data[0][0])
             else:
                 print("\nNo data exist!")
         elif choose == 2:
-            cur.execute("select * from students where gpu > (select avg(gpu) from students)")
+            cur.execute("select * from students where gpa > (select avg(gpa) from students)")
             data = cur.fetchall()
             for datas in data:
                 print(datas)
-            cur.execute("select count(ids) from students where gpu > (select avg(gpu) from students order by gpu)")
+            cur.execute("select count(ids) from students where gpa > (select avg(gpa) from students order by gpa)")
             cdata = cur.fetchall()
             print("\nNumber of students: ",cdata[0][0])
         else:
@@ -103,13 +128,14 @@ print("\nHello and welcome to university system")
 while True:
     print("----------------\nPlease choose your option:")
     print("1.Insert student")
-    print("2.Calculate gpu")
+    print("2.Calculate GPU")
     print("3.Add database")
     print("4.Show all students")
-    print("5.Delete student")
-    print("6.Students count")
-    print("7.Average of university")
-    print("8.Exit\n----------------\n")
+    print("5.Search students")
+    print("6.Delete student")
+    print("7.Students count")
+    print("8.Average of university")
+    print("9.Exit\n----------------\n")
     choice = int(input("Your choice:"))
     print('\n')
     match choice:
@@ -120,35 +146,44 @@ while True:
             studentlastname = input("Please enter student last name: ")
             while not flag:
                 flag = True
+                studentcourses = []
                 studentgrades = []
+                sc = input("Please enter students courses: ").split()
                 sg = list(map(int, input("Please enter student grades: ").split()))
-                for i in sg:
-                    if i >= 0 and i <= 20:
-                        studentgrades.append(i)
-                    else:
-                        print("Invalid input!\nPlease try again\n")
-                        flag = False
-                        break
-                if flag:
-                    unisys.insert(studentid, studentname, studentlastname, studentgrades)
+                if len(sc) == len(sg):
+                    for i in sc:
+                        studentcourses.append(i)
+                    for i in sg:
+                        if i >= 0 and i <= 20:
+                            studentgrades.append(i)
+                        else:
+                            print("Invalid input!\nPlease try again\n")
+                            flag = False
+                            break
+                    if flag:
+                        unisys.insert(studentid, studentname, studentlastname, studentgrades, studentcourses)
+                else:
+                    print("Grades over then courses please try again\n")
         case 2:
-            unisys.calculategpu()
+            unisys.calculategpa()
         case 3:
             unisys.adddatabase()
         case 4:
             unisys.showallstudents()
         case 5:
+            unisys.searchstudents()
+        case 6:
             delstudent = input("Please enter student id for delete: ")
             unisys.deletestudent(delstudent)
-        case 6:
-            unisys.countofstudents()
         case 7:
+            unisys.countofstudents()
+        case 8:
             print("Choose your option: ")
             print("1.Show Academic average of the university")
             print("2.Show the GPA of students who are higher than the academic GPA of the university\n")
             choose = int(input("Your option: "))
             unisys.averageofuniversity(choose)
-        case 8:
+        case 9:
             cur.close()
             conn.close()
             print("BYE!")
